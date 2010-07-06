81a87684069ade1e7a8d30ebe7eb6b87ef48c965
sathish <sathish@sathish-eeepc.(none)>
2010-07-07 01:39:02 +0530

    fixnum/float mixins to represent or convert metric units of length

---
+++ lib/uom.rb
@@ -0,0 +1,19 @@
+require 'uom/to_meter'
+
+class Fixnum
+	include Uom::ToMeter
+
+end
+
+class Float
+  include Uom::ToMeter
+
+  def method_missing(m,*args)
+    if m.to_s =~ /to\_(\w+)/
+      conversion_factor = 1.send($1)
+      to_f / conversion_factor
+    else
+      super
+    end
+  end
+end
